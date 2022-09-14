a3ec23b65f1a9c8e79514c42e26f54860503e3e7
PsyByACET <web-47@mail.ru>
2022-09-14 13:24:07 +0300

    12 lessons

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://cdn.mtgame.ru/logo_sber.png" alt=""/>
+        </header>
+    );
+}
+export default Header
---
+++ src/components/Profile.js
@@ -0,0 +1,25 @@
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://browsecat.net/sites/default/files/3440x1440-mountain-wallpapers-98041-878818-4684520.png" alt=""/>
+            </div>
+            <div>
+                ava+des
+            </div>
+            <div>
+                my posts
+                <div className="div">
+                    new post
+                </div>
+                <div>
+                    post#1
+                </div>
+                <div>
+                    post#2
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Profile
